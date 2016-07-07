83b256f72410de410ac3d5e890a6999a89c22f95
SigridZhangSY <sigridzhang0603@163.com>
2016-07-07 17:33:20 +0800

    ‘test’

---
+++ src/test/java/com/thoughtworks/test1.java
@@ -0,0 +1,18 @@
+package com.thoughtworks;
+
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by syzhang on 7/7/16.
+ */
+
+
+public class test1 {
+    @Test
+    public void testss(){
+        assertThat(1, is(1));
+    }
+}
